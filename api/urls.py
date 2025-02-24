94b66c16f5587d6784d6c96ce4f0718f30043f34
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-02-24 15:43:44 +0000

    sync changes

--- api/urls.py
+++ api/urls.py
@@ -10,5 +10,13 @@ urlpatterns = [
     path("", views.BrandSuggestionAPIView.as_view()),
     path("bank-contacts/", views.ContactView.as_view(), name="contacts"),
     path("bank/", views.BrandsView.as_view(), name="bank"),
+<<<<<<< HEAD
     path("banks/<int:bank_id>/", get_bank_commentary, name="get_bank_commentary"),
+=======
+    path(
+        "bank/<int:brand_id>/feature_override/",
+        views.BrandFeatureOverride.as_view(),
+        name="brand_feature_override",
+    ),
+>>>>>>> 7b4cd04da2fb31b47755f9090beecb8cc0df7d78
 ]
--- brand/models/brand.py
+++ brand/models/brand.py
@@ -10,6 +10,8 @@ from cities_light.models import Region, SubRegion
 from django_countries.fields import CountryField
 from model_utils.models import TimeStampedModel
 
+import datasource.models as dsm
+from datasource.constants import lev_distance, model_names
 
 # from Levenshtein import distance as lev
 
